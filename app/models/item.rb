8b9a434eab2d93bf11b6398461dfa9cd0e799303
debzow <david.gerard.42@gmail.com>
2018-11-26 17:59:13 +0100

    create table cart, associations between table cart,user, item and seed of table cart,user

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
     validates :description, presence: true
     validates :price, presence: true
     validates :image, presence: true
+
+    has_and_belongs_to_many :carts
 end
