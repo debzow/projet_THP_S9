3de7315a42c18fa9b51987f814764348f63f2804
Javibz <ramonjavi8@gmail.com>
2018-11-27 21:07:38 +0100

    indentation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,5 @@ class HomeController < ApplicationController
 
   def show
   	@item_id = Item.find(params[:id])
-
-  	puts "LOOOOOOOOOK HEEEEEREEEEEEEE"
-  	puts params 
-  	puts "@item_id : #{@item_id}" 
-  	puts @item_id.image
-  
   end
 end
