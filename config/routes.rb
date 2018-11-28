d7c4147f6bce805cb678c7d583d4559f9fa8d56d
Vano <hervelee99@gmail.com>
2018-11-28 14:01:49 +0100

    credentials prob

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
 Rails.application.routes.draw do
-
-  #Carts routes
-  get 'carts/show', to:'carts#show'
-  post 'carts/create', to:'carts#create'
-  post 'carts/add_item', to:'carts#add_item'
-  post 'carts/remove_item', to:'carts#remove_item'
-  post 'carts/reset', to:'carts#reset'
-
-  get'/store', to:'store#index'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  resources :charges
-  resources :home
 end
